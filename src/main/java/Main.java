35d8b6fc9a2bc7a9909a7a562d3b4e658ea6e873
powerlord1990 <fatik911turbo1@gmail.com>
2019-09-22 22:50:44 +0300

    Дз 2 лекция 2 задача

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Tariff MyOnline = new Tariff(290, 400, 15, "Соц сети, Безлимит Теле2 Россия");
+        Tariff MyOnlinePlus = new Tariff(350, 600, 20, "Соц сети,  Безлимит Теле2 Россия, " +
+                "интернет за границей");
+        Tariff MyTalk = new Tariff(190, 250, 3, "Безлимит Теле2 Россия");
+
+    }
+}
---
+++ src/main/java/Tariff.java
@@ -0,0 +1,14 @@
+public class Tariff {
+    double cost;
+    double minutes;
+    double internet;
+    String additionalServices;
+
+    Tariff(double c, double m, double i, String aS) {
+        cost = c;
+        minutes = m;
+        internet = i;
+        additionalServices = aS;
+
+
+    }
