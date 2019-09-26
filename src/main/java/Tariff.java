244458fec625b6cbd4b6535e03b033c5ae6e7927
powerlord1990 <fatik911turbo1@gmail.com>
2019-09-26 20:58:17 +0300

    fix #6

--- src/main/java/Tariff.java
+++ src/main/java/Tariff.java
@@ -1,13 +1,13 @@
 public class Tariff {
-    double cost;
-    double minutes;
-    double internet;
+    double tariffCost;
+    double tariffMinutes;
+    double tariffInternet;
     String additionalServices;
 
-    Tariff(double c, double m, double i, String aS) {
-        cost = c;
-        minutes = m;
-        internet = i;
+    Tariff(double cost, double minutes, double internet, String aS) {
+        cost = cost;
+        minutes = minutes;
+        internet = internet;
         additionalServices = aS;
 
 
