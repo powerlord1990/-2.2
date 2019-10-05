65ff743a154109d42e0b7333b7331055b2266218
powerlord1990 <fatik911turbo1@gmail.com>
2019-10-05 15:10:55 +0300

    fix #10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Tariff MyOnline = new Tariff(2211,"Мой онлайн", false,290,400,15,
+        Tariff myOnline = new Tariff(2211,"Мой онлайн", false,290,400,15,
                 0, true,true,true);
 
-        Tariff MyOnlinePlus = new Tariff(2212,"Мой онлайн+", false,350,600,
+        Tariff myOnlinePlus = new Tariff(2212,"Мой онлайн+", false,350,600,
                 20,0,true,true,true);
 
-        Tariff MyTele2 = new Tariff(2213,"Мой Tele2", true,7,0,
+        Tariff myTele2 = new Tariff(2213,"Мой Tele2", true,7,0,
                 6,0,true,false,false);
 
     }
